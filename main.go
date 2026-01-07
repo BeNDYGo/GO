f51adfa9198f501f325327741161126c503cac29
BeNDYGo <palexascha@gmail.com>
2026-01-07 20:25:10 +0300

    -

--- main.go
+++ main.go
@@ -2,44 +2,31 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
-type Auto interface{
-	Drive(length int)
-}
+var c int = 0
 
-type BMW struct{
-		Year  int
-		Color string
-		Fuel int
-}
-
-type Tesla struct{
-		Year  int
-		Color string
-		Pover int
-}
-
-func (b *BMW) Drive(length int){
-	b.Fuel -= length
-	fmt.Println("BMW едет на расстояние", length, "km, осталось топлива:", b.Fuel, "л")
-}
-
-func (t *Tesla) Drive(length int){
-	t.Pover -= length
-	fmt.Println("Tesla едет на расстояние", length, "km, осталось заряда:", t.Pover, "киловатт-час")
+func f(wg *sync.WaitGroup){
+	defer wg.Done()
+	for i := 1 ;i <= 1000; i++{
+		c++
+	}
 }
 
 func main(){
-	autos := []Auto{
-		&BMW{Fuel: 500},
-		&Tesla{Pover: 300},
-	}
+	wg := &sync.WaitGroup{}
 
-	lengths := []int{130, 170}
+	wg.Add(6)
 
-	for i, auto := range autos {
-		fmt.Printf("Автомобиль %d: ", i)
-		auto.Drive(lengths[i])
-	}
+	go f(wg)
+	go f(wg)
+	go f(wg)
+	go f(wg)
+	go f(wg)
+	go f(wg)
+
+	wg.Wait()
+
+	fmt.Printf("Значение i: %v ", c)
 }
